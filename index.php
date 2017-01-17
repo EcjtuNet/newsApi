9af6f344912baaed4dd2c76b112fee901858022c
superkevingit <superkevingit@gmail.com>
2017-01-17 14:27:56 +0800

    fix construction

--- index.php
+++ index.php
@@ -1,8 +1,10 @@
 <?php
-	use \Psr\Http\Message\ServerRequestInterface as Request;
-	use \Psr\Http\Message\ResponseInterface as Response;
 	require_once 'vendor/autoload.php';
 	require_once 'config.php';
+
+	use \Psr\Http\Message\ServerRequestInterface as Request;
+	use \Psr\Http\Message\ResponseInterface as Response;
+
 	$medoo=new medoo([
 			'database_type' =>$db['database_type'],
 		    'database_name' =>$db['database_name'],
@@ -11,26 +13,31 @@
 		    'password' =>$db['password'],
 		    'charset' =>$db['charset']
 	]);
-$app = new \Slim\App;
-$app->get('/list', function ($request,$response,$args=[]) use($medoo){
-	if(isset($request->getQueryParams()['pageNum'])){
-		$pageNum=$request->getQueryParams()['pageNum'];
-	}else{
-		$pageNum=3;
-	}
-	if(isset($request->getQueryParams()['page'])){
-		$page=$request->getQueryParams()['page'];
-	}else{
-		$page=1;
-	}
-	$page<=0?1:$page;
-	$pageNum<=0?3:$pageNum;
-	$offset = ($page - 1) * $pageNum;
-	$condition['LIMIT'] = array($offset, $pageNum);
-	$condition['ORDER'] = 'mynews.senddate DESC';
-    $datas = $medoo->select("addonarticle", [ "[<]mynews" => ["aid" => "aid"] ],["addonarticle.aid","addonarticle.typeid","addonarticle.redirecturl","mynews.title","mynews.writer","mynews.senddate","mynews.body"],$condition);
-	$count=count($datas);
-	echo json_encode(array('count'=>$count,'list'=>$datas));
-	return  $response->withHeader( 'Content-Type','application/json');
+
+    $app = new \Slim\App;
+    $app->get('/list', function ($request,$response,$args=[]) use($medoo){
+	    if(isset($request->getQueryParams()['pageNum'])){
+		    $pageNum=$request->getQueryParams()['pageNum'];
+	    }else{
+		    $pageNum=3;
+	    }
+
+	    if(isset($request->getQueryParams()['page'])){
+		    $page=$request->getQueryParams()['page'];
+	    }else{
+		    $page=1;
+	    }
+
+	    $page<=0?1:$page;
+    	$pageNum<=0?3:$pageNum;
+	    $offset = ($page - 1) * $pageNum;
+	    $condition['LIMIT'] = array($offset, $pageNum);
+	    $condition['ORDER'] = 'mynews.senddate DESC';
+
+        $datas = $medoo->select("addonarticle", [ "[<]mynews" => ["aid" => "aid"] ],["addonarticle.aid","addonarticle.typeid","addonarticle.redirecturl","mynews.title","mynews.writer","mynews.senddate","mynews.body"],$condition);
+	    $count=count($datas);
+
+        return $response->withJson(array('count'=>$count,'list'=>$datas));
 });
-$app->run();
+
+    $app->run();
